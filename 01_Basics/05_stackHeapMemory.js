e258bc69354d197b6080a0af965b7d4a9e4aab93
RAJNEESHMIST <116701705+RAJNEESHMIST@users.noreply.github.com>
2023-12-13 05:00:08 +0000

    stack

---
+++ 01_Basics/05_stackHeapMemory.js
@@ -0,0 +1,10 @@
+//this program os to understand the various concept of the memory used in the program 
+//1.stack memory - stack memory is used by the primitive data type 
+//in the stack memory a copy instead of refernce or orginal value of variable is passed to do operation on it so if we change in the value the change not happened in tha original value
+//for example
+
+let myName="Rajneesh verma";
+
+let Name=myName;
+
+console.log(Name)
